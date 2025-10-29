32d41a0341b6dec63fb6b0df6d08eb7d854af8da
francivaldo4334 <francivaldodev@gmail.com>
2025-10-29 20:37:53 -0300

    :sparkles: feat: adiciona primeira versão de modelos do banco

--- app/acervo_de_livros/models.py
+++ app/acervo_de_livros/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Autor(models.Model):
+    nome = models.CharField(
+        verbose_name="Nome do autor",
+    )
+
+
+class TipoLivro(models.Model):
+    tipo = models.CharField(
+        verbose_name="Nome do tipo de livro",
+        help_text="Indica o tipo de livro ex: Quadrinho, Crônica brasileira, ect",
+    )
+
+
+class LivroRegistro(models.Model):
+    quantidade = models.PositiveIntegerField(
+        verbose_name="Quantidade de cópias",
+        validators=[
+            MinValueValidator(1),
+        ],
+    )
+    autores = models.ManyToManyField(Autor)
+    tipo = models.ForeignKey(
+        TipoLivro,
+        on_delete=models.CASCADE,
+    )
+    edicao = models.PositiveIntegerField(
+        verbose_name="Número da edição",
+        validators=[
+            MinValueValidator(1),
+        ],
+    )
+    data_publicacao = models.DateField(
+        verbose_name="Data da públicação do livro",
+    )
+    isbn = models.CharField(
+        verbose_name="Código ISBN do livro",
+        help_text="Digite um ISBN válido (ex: 978-85-12345-67-8 ou 85-12345-67-X)",
+        max_length=20,
+        unique=True,
+    )
