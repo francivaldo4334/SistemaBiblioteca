68615895c0b31bc2df343a4461844a9ee0edefb3
francivaldo4334 <francivaldodev@gmail.com>
2025-11-07 08:34:05 -0300

    :construction: configura rotas de api do restframework

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "acervo_de_livros",
     "controle_de_emprestimo",
     "controle_de_contas",
--- app/app/urls.py
+++ app/app/urls.py
@@ -22,4 +22,5 @@ import acervo_de_livros.urls as acervo_de_livros_urls
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("acervo_de_livros/", include(acervo_de_livros_urls)),
+    path('api-auth/', include('rest_framework.urls'))
 ]
